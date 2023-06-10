625ce7930c5a3b6355b254e45adf6e26eac8440d
yassine542 <101680065+yassine542@users.noreply.github.com>
2023-06-10 20:52:16 +0100

    ex03

--- CPP_01/ex03/HumanA.cpp
+++ CPP_01/ex03/HumanA.cpp
@@ -6,7 +6,7 @@
 /*   By: yajallal <yajallal@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 11:20:44 by yajallal          #+#    #+#             */
-/*   Updated: 2023/06/10 20:49:37 by yajallal         ###   ########.fr       */
+/*   Updated: 2023/06/10 20:52:15 by yajallal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,3 @@ void HumanA::attack()
 {	
 	std::cout << name << " attacks with their " << weapon->getType() << std::endl;
 }
-
